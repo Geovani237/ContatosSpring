a17e27fe9576d5a4b7d8f437f77940b95fbf3994
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2025-04-29 22:54:51 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/contatosSpring/model/Contato.java
@@ -0,0 +1,57 @@
+package br.com.fiap.contatosSpring.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tbl_contatos")
+public class Contato {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "CONTATOS_SEQ"
+    )
+    @SequenceGenerator(
+            name = "CONTATOS_SEQ",
+            sequenceName = "CONTATOS_SEQ",
+            allocationSize = 1
+    )
+    private Long id;
+    private String nome;
+    private String email;
+    private LocalDate dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
---
+++ src/test/java/br/com/fiap/contatosSpring/ContatosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.contatosSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
